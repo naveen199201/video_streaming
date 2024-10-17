b5138d3d0108e9db72c64c58a637caf64b1f5fcf
naveen199201 <naveen199201@gmail.com>
2024-10-17 23:15:44 +0530

    changed DEBUG to false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y0h2&_$u6jm!6h^9p&vh=_fe&oqlbf%og)ug(&=vevxs378ks('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost','*']
 
